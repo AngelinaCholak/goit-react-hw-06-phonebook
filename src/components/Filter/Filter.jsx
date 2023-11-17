f08f8a79fbc2bfcca3352a9e776647b64e3b77b2
AngelinaCholak <127952572+AngelinaCholak@users.noreply.github.com>
2023-11-18 00:26:11 +0400

    hw

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,13 +1,13 @@
-
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setFilter } from 'redux/contact/filter.reducer';
+
 
 import css from './Filter.module.css';
 
 const Filter = () => {
   const dispatch = useDispatch();
-  const filter = useSelector(state => state.filterReducer);
+  const filter = useSelector(state => state.filter);
 
   const handleChange = event => {
     dispatch(setFilter(event.target.value));
--- src/redux/contact/filter.reducer.js
+++ src/redux/contact/filter.reducer.js
@@ -1,15 +1,14 @@
-const initialState = '';
+import { createSlice } from '@reduxjs/toolkit';
 
-export const filterReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'SET_FILTER':
-      return action.payload;
-    default:
-      return state;
-  }
-};
-
-export const setFilter = filter => ({
-  type: 'SET_FILTER',
-  payload: filter,
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: '',
+  reducers: {
+    setFilter(state, action) {
+      return action.payload.toLowerCase();
+    },
+  },
 });
+
+export const { setFilter } = filterSlice.actions;
+export default filterSlice.reducer;
