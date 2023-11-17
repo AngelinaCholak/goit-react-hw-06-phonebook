07ca7cae74510df2f50e3560470402a8fd3fcef9
AngelinaCholak <127952572+AngelinaCholak@users.noreply.github.com>
2023-11-17 18:29:06 +0400

    hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,80 +1,67 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 import PhoneBook from './PhoneBook/PhoneBook';
 import Contacts from './Contacts/Contacts';
 import Filter from './Filter/Filter';
 import css from './App.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { addContact, deleteContact } from 'redux/contact/contact.reducer';
 
 export const App = () => {
-  
-  const initialState = [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ];
 
-  const [contacts, setContacts] = useState(() => {
-    return JSON.parse(localStorage.getItem('contacts')) || initialState;
-  });
-  
+  const contacts = useSelector(state => state.contactsStore.contacts);
+  const dispatch = useDispatch();
+
   const [filter, setFilter] = useState('');
 
-  useEffect(() => {
-    const stringifiedContacts = JSON.stringify(contacts);
-      localStorage.setItem('contacts', stringifiedContacts);
-  }, [contacts]);
+    const handleAddContact = contactData => {
+      const hasDuplicates = contacts.some(
+        contact => contact.name.toLowerCase() === contactData.name.toLowerCase()
+      );
 
+      if (hasDuplicates) {
+        alert(`Oops, produc with title '${contactData.title}' already exists!`);
+        return;
+      }
 
-  const handleAddContact = contactData => {
-    const hasDuplicates = contacts.some(
-      contact => contact.name.toLowerCase() === contactData.name.toLowerCase()
-    );
+      const finalContact = {
+        ...contactData,
+        id: nanoid(),
+      };
 
-    if (hasDuplicates) {
-      alert(`Contact with the name ${contactData.name} already exists.`);
-      return;
-    }
-
-    const finalContact = {
-      ...contactData,
-      id: nanoid(),
+      dispatch(addContact(finalContact));
     };
+  
 
-    setContacts(prevContacts => [...prevContacts, finalContact]);
-  };
+  const handleDeleteContact = contactId => {
+    dispatch(deleteContact(contactId));
 
-  const handleDeleteContact = id => {
-    const updatedContacts = contacts.filter(contact => contact.id !== id);
-      setContacts(updatedContacts);
   };
- 
 
   const handleFilterChange = filter => {
     setFilter(filter);
   };
 
- const getFilteredContacts = () => {
+  const getFilteredContacts = () => {
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
 
-    const filteredContacts = getFilteredContacts();
+  const filteredContacts = getFilteredContacts();
 
-    return (
-      <div className={css.container}>
-        <h1 className={css.title}>Phonebook</h1>
-        <PhoneBook handleAddContact={handleAddContact} />
-        <h2>Contacts</h2>
-        <Filter filter={filter} handleFilterChange={handleFilterChange} />
-        <Contacts
-          contacts={filteredContacts}
-          handleDeleteContact={handleDeleteContact}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className={css.container}>
+      <h1 className={css.title}>Phonebook</h1>
+      <PhoneBook handleAddContact={handleAddContact} />
+      <h2>Contacts</h2>
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
+      <Contacts
+        contacts={filteredContacts}
+        handleDeleteContact={handleDeleteContact}
+      />
+    </div>
+  );
+};
+
 export default App;
-
-
